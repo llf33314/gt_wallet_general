c42d5d80d025bc9c0e3b968d2b4b22cc766d17f2
sky <Administrator@GtTSer014>
2017-12-12 18:19:11 +0800

    啊啊速度发斯蒂芬

--- java/wallet/wallet-web/src/test/java/com/gt/wallet/AddressTest.java
+++ java/wallet/wallet-web/src/test/java/com/gt/wallet/AddressTest.java
@@ -8,11 +8,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSONObject;
 import com.gt.api.dto.ResponseUtils;
-import com.gt.api.util.HttpClienUtils;
 import com.gt.api.util.RequestUtils;
 import com.gt.api.util.httpclient.JsonUtil;
 import com.gt.wallet.utils.WalletWebConfig;
+import com.gt.wallet.utils.httpclient.WalletHttpClienUtil;
 
 
 @RunWith( SpringJUnit4ClassRunner.class )
@@ -25,7 +26,8 @@ public class AddressTest extends BasicTest {
     	ResponseUtils<List<Map<String, Object>>> serverResponse=null;
 		RequestUtils<Integer> requestUtils=new RequestUtils<>();
 		requestUtils.setReqdata(2);	
-		serverResponse= 	HttpClienUtils.reqPostUTF8(JsonUtil.toJSONString(requestUtils), "http://yifriend.net/8A5DA52E/shopapi/6F6D9AD2/79B4DE7C/queryCityByLevel.do", ResponseUtils.class, WalletWebConfig.getWxmpKey());
+		
+		serverResponse= 	WalletHttpClienUtil.reqPostUTF8(JSONObject.toJSONString(requestUtils), "http://127.0.0.1:8082/8A5DA52E/shopapi/6F6D9AD2/79B4DE7C/queryCityByLevel.do", ResponseUtils.class, WalletWebConfig.getWxmpKey());
 		System.out.println(JsonUtil.toJSONString(serverResponse));
     }
 
