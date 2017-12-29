81b2693ee0baefe92332abe5e6b37691890cdeab
sky <13528307867@163.com>
2017-12-29 09:45:48 +0800

    bug修复

--- java/wallet/wallet-service/src/main/java/com/gt/wallet/service/impl/member/WalletMemberServiceImpl.java
+++ java/wallet/wallet-service/src/main/java/com/gt/wallet/service/impl/member/WalletMemberServiceImpl.java
@@ -129,8 +129,8 @@ public class WalletMemberServiceImpl extends BaseServiceImpl<WalletMemberMapper,
 					if(CommonUtil.isNotEmpty(serverResponse.getData())){
 						walletMembers.get(i).setWalletCompany(serverResponse.getData());
 						walletMembers.get(i).getWalletCompany().setLegalPhone(PhoneUtil.hide(WalletKeyUtil.getDesString(walletMembers.get(i).getWalletCompany().getLegalPhone())));
-						walletMembers.get(i).getWalletCompany().setLegalIds(IdCardUtil.hide(WalletKeyUtil.getDesString(walletMembers.get(i).getWalletCompany().getLegalIds())));
-						walletMembers.get(i).getWalletCompany().setAccountNo(BankUtil.hide(WalletKeyUtil.getDesString(walletMembers.get(i).getWalletCompany().getAccountNo())));
+						walletMembers.get(i).getWalletCompany().setLegalIds(IdCardUtil.hide(YunSoaMemberUtil.rsaDecrypt(walletMembers.get(i).getWalletCompany().getLegalIds())));
+						walletMembers.get(i).getWalletCompany().setAccountNo(BankUtil.hide(YunSoaMemberUtil.rsaDecrypt(walletMembers.get(i).getWalletCompany().getAccountNo())));
 					}
 				}
 			}
