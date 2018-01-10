533ffc03e7ccc45f85d5247810db01c8663573ff
gzhidao1010@gmail.com <gzhidao1010@gmail.com>
2018-01-10 10:28:37 +0800

    qianbao

--- cli-admin/src/components/wallet/vue/index.vue
+++ cli-admin/src/components/wallet/vue/index.vue
@@ -6,130 +6,154 @@
 <script>
 export default {
   mounted() {
-    this.isPass()
+    this.isPass();
   },
   methods: {
     // 查询会员信息
     getVipMsg() {
-      console.log(6666)
+      console.log(6666);
       $.ajax({
-        url: this.DFPAYDOMAIN + '/walletMember/findMember',
-        type: 'GET',
-        dataType: 'json',
-        success: (res) => {
-          console.log(res, '查询会员信息')
+        url: this.DFPAYDOMAIN + "/walletMember/findMember",
+        type: "GET",
+        dataType: "json",
+        success: res => {
+          console.log(res, "查询会员信息");
           // 开通
           // res.data.status 会员状态(-3：审核不通过，2:删除,-1:锁定用户,0:创建,1:审核中,3:正常使用)
           if (res.code == 0) {
-            if (res.data.memberType == 3) { //个人会员
-              if (res.data.status == 0 || res.data.status == -1) { //创建
-                if (!(res.data.walletIndividual) || res.data.walletIndividual.name == '') { //步骤1
+            if (res.data.memberType == 3) {
+              //个人会员
+              if (res.data.status == 0 || res.data.status == -1) {
+                //创建
+                const individual = res.data.walletIndividual;
+                if (!individual) {
+                  //步骤1
                   this.$router.push({
-                    path: '/wallet/individual/open/' + res.data.id
-                  })
-                }
-                if (res.data.isBindingPhone == 0) { //步骤2
+                    path: "/wallet/individual/open/" + res.data.id
+                  });
+                } else if (res.data.isBindingPhone == 0 && individual) {
+                  //步骤2
                   this.$router.push({
-                    path: '/wallet/individual/open/bindPhone/' + res.data.id
-                  })
-                }
-                if (res.data.isBindingPhone == 1 && res.data.status == -1) {  //正常使用
-                  window.sessionStorage.walletId = res.data.id
+                    path: "/wallet/individual/open/bindPhone/" + res.data.id
+                  });
+                } else if (
+                  res.data.isBindingPhone == 1 &&
+                  res.data.status == -1
+                ) {
+                  //正常使用
+                  window.sessionStorage.walletId = res.data.id;
                   this.$router.push({
-                    path: '/wallet/individual/index'
-                  })
+                    path: "/wallet/individual/index"
+                  });
                 }
-              } else if (res.data.status == 3) { //正常使用
-                window.sessionStorage.walletId = res.data.id
+              } else if (res.data.status == 3) {
+                //正常使用
+                window.sessionStorage.walletId = res.data.id;
                 this.$router.push({
-                  path: '/wallet/individual/index'
-                })
+                  path: "/wallet/individual/index"
+                });
               }
-            } else { //企业会员
-              if (res.data.status == 0 || res.data.status == -1) {  //创建
-                if (!(res.data.walletCompany) || res.data.walletCompany.companyName == '') { //步骤1
+            } else {
+              //企业会员
+              if (res.data.status == 0 || res.data.status == -1) {
+                //创建
+                if (
+                  !res.data.walletCompany ||
+                  res.data.walletCompany.companyName == ""
+                ) {
+                  //步骤1
                   this.$router.push({
-                    path: '/wallet/company/open/base/' + res.data.id
-                  })
+                    path: "/wallet/company/open/base/" + res.data.id
+                  });
                 }
-                if (res.data.walletCompany.doBusinessUrl == '') { //步骤2
+                if (res.data.walletCompany.doBusinessUrl == "") {
+                  //步骤2
                   this.$router.push({
-                    path: '/wallet/company/open/uploadFil/' + res.data.id
-                  })
+                    path: "/wallet/company/open/uploadFil/" + res.data.id
+                  });
                 }
-                if (res.data.isBindingPhone == 0) { //步骤3
+                if (res.data.isBindingPhone == 0) {
+                  //步骤3
                   this.$router.push({
-                    path: '/wallet/company/open/bindPhone/' + res.data.id
-                  })
+                    path: "/wallet/company/open/bindPhone/" + res.data.id
+                  });
                 }
-                if (res.data.isBindingPhone == 1 && res.data.status == -1) { //正常使用
-                  window.sessionStorage.walletId = res.data.id
+                if (res.data.isBindingPhone == 1 && res.data.status == -1) {
+                  //正常使用
+                  window.sessionStorage.walletId = res.data.id;
                   this.$router.push({
-                    path: '/wallet/company/index'
-                  })
+                    path: "/wallet/company/index"
+                  });
                 }
               }
-              if (res.data.status == 3) {  //正常使用
-                window.sessionStorage.walletId = res.data.id
+              if (res.data.status == 3) {
+                //正常使用
+                window.sessionStorage.walletId = res.data.id;
                 this.$router.push({
-                  path: '/wallet/company/index'
-                })
+                  path: "/wallet/company/index"
+                });
               }
             }
-          } else if (res.code == 1009) { // 未开通
+          } else if (res.code == 1009) {
+            // 未开通
             this.$router.push({
-              path: '/wallet/noOpen'
-            })
-          } else { //异常的错误
-            this.$message.error(res.msg)
+              path: "/wallet/noOpen"
+            });
+          } else {
+            //异常的错误
+            this.$message.error(res.msg);
           }
         }
-      })
+      });
     },
     goToWhere(data, id) {
-      var g = [data.doBusinessUrl, data.identitycardUrl1, data.identitycardUrl2, data.licenseUrl];
-      var flag = false
+      var g = [
+        data.doBusinessUrl,
+        data.identitycardUrl1,
+        data.identitycardUrl2,
+        data.licenseUrl
+      ];
+      var flag = false;
       g.forEach(item => {
-        if (item == null || item == '') {
-          flag = true
+        if (item == null || item == "") {
+          flag = true;
         }
-      })
+      });
       if (flag) {
         this.$router.push({
-          path: '/wallet/company/open/uploadFile/' + id
-        })
+          path: "/wallet/company/open/uploadFile/" + id
+        });
       } else {
         this.$router.push({
-          path: '/wallet/company/open/base/' + id
-        })
+          path: "/wallet/company/open/base/" + id
+        });
       }
     },
     // 判断商家是否开通多粉钱包
     isPass() {
       $.ajax({
-        url: this.DFPAYDOMAIN + '/walletMember/isPassWallet',
-        type: 'GET',
-        dataType: 'json',
-        success: (res) => {
-          console.log(res, '判断商家是否开通多粉钱包')
+        url: this.DFPAYDOMAIN + "/walletMember/isPassWallet",
+        type: "GET",
+        dataType: "json",
+        success: res => {
+          console.log(res, "判断商家是否开通多粉钱包");
           //0 :已开通, 1:未开通
           if (res.code == 0) {
             if (res.data == 0) {
-              this.getVipMsg()
+              this.getVipMsg();
             } else {
               this.$router.push({
-                path: '/wallet/noOpen'
-              })
+                path: "/wallet/noOpen"
+              });
             }
           } else {
-            this.$message.error(res.msg)
+            this.$message.error(res.msg);
           }
         }
-      })
+      });
     }
   }
-}
-
+};
 </script>
 <style lang="less">
 
