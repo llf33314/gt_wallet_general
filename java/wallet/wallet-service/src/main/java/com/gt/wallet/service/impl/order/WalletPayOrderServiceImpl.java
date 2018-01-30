f0a3249f2205b0fb8e9071bbe99320c1fc64035e
sky <13528307867@163.com>
2018-01-30 19:26:28 +0800

    1111111111111

--- java/wallet/wallet-service/src/main/java/com/gt/wallet/service/impl/order/WalletPayOrderServiceImpl.java
+++ java/wallet/wallet-service/src/main/java/com/gt/wallet/service/impl/order/WalletPayOrderServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.hadoop.mapred.gethistory_jsp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
