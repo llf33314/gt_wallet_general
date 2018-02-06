d2b848048990cf8fffaf1fbc6f9c48bcb06b75b7
sky <13528307867@163.com>
2018-02-06 14:22:41 +0800

    11111111111111111111111111

--- java/wallet/wallet-service/src/main/java/com/gt/wallet/service/impl/order/WalletPayOrderServiceImpl.java
+++ java/wallet/wallet-service/src/main/java/com/gt/wallet/service/impl/order/WalletPayOrderServiceImpl.java
@@ -409,7 +409,7 @@ public class WalletPayOrderServiceImpl extends BaseServiceImpl<WalletPayOrderMap
 			throw new BusinessException("异常：订单号不存在!");
 		}
 		WalletPayOrder walletPayOrder=serverResponseWalletPayOrder.getData();
-		if(walletPayOrder.getTakeState()==3||walletPayOrder.getReturnStatus()==2){//金额已划入商检余额或手续费已返佣
+		if(walletPayOrder.getTakeState()==3){//金额已划入商检余额或手续费已返佣
 			log.error("biz refund api fail：order is  finish");
 			throw new BusinessException("订单号已完成，不可退款");
 		}
